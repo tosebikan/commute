1211f2473be594a9840101782b6f5289a53ea54c
tosebikan <tosebikan@gmail.com>
2021-01-02 23:13:38 +0000

    add linter

---
+++ screens/onboarding/OnBoarding.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
+function Onboarding() {
+  return (
+    <SafeAreaView>
+      <Text>OnBoarding Screen</Text>
+    </SafeAreaView>
+  );
+}
+export default Onboarding;
