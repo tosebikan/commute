45154cacc2deaecfe93179f1e23589336ca885f1
tosebikan <tosebikan@gmail.com>
2021-01-03 22:33:16 +0000

    add onboarding data

--- screens/onboarding/OnBoarding.js
+++ screens/onboarding/OnBoarding.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
-import { COLORS } from '../../constants';
+import { SafeAreaView, StyleSheet, Animated } from 'react-native';
+
+// constants
+import { images, theme } from '../../constants';
+const { onboarding1, onboarding2, onboarding3 } = images;
+
+// theme
+const { COLORS, SIZES } = theme;
+
+// Dummy Data
+const onBoarding = [
+  {
+    title: 'Commute with ease',
+    description:
+      'best way to find people moving to similar destinations as you',
+    img: 'onboarding1'
+  },
+  {
+    title: 'Commute safe',
+    description:
+      'Same trip multiple times with poeple living in your neighbourhood',
+    img: 'onboardings'
+  },
+  {
+    title: 'Commute cheaply',
+    description:
+      'Monthly payment to handle trips to recurring destinations everyday',
+    img: 'onboarding3'
+  }
+];
+
+function renderContent() {
+  <Animated.ScollView>{}</Animated.ScollView>;
+}
 
 function OnBoarding() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>OnBoarding Screen</Text>
-    </SafeAreaView>
+    <SafeAreaView style={styles.container}>{renderContent()}</SafeAreaView>
   );
 }
 export default OnBoarding;
