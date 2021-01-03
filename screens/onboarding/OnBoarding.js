c749c1f62f715b93e958acdc57324cc28e2df1ed
tosebikan <tosebikan@gmail.com>
2021-01-03 22:23:10 +0000

    add reanimated library

--- screens/onboarding/OnBoarding.js
+++ screens/onboarding/OnBoarding.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { COLORS } from '../../constants';
 
 function OnBoarding() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>OnBoarding Screen</Text>
     </SafeAreaView>
   );
 }
 export default OnBoarding;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.white
+  }
+});
